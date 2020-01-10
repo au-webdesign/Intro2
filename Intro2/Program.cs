14fee7578979491c88adec46c605d73b5ab6023d
david lash <dlash02@gmail.com>
2020-01-10 09:15:33 -0600

    Add project files.

---
+++ Intro2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Intro2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myList = new int[10];
+
+            for ( int i=0; i<10; i++) {
+                Console.Write("\n i={0}", i);
+                myList[i] = i * i;
+            }
+            for (int i = 0; i < 10; i++){
+                Console.Write("\n i={0}", myList[i]);
+                
+            }
+        }
+    }
+}
